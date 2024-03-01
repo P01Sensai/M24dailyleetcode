65b0aa4cdd1f788a66f6318b9989e07f9b3fd6f5
Pramanshu Prajapati <89986458+P01Sensai@users.noreply.github.com>
2024-03-02 00:10:34 +0530

    Update 1march.cpp
    adding approach2

--- 1march.cpp
+++ 1march.cpp
@@ -1,8 +1,9 @@
+// This the approach 1 - :
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
 
-      // This the approach:
+      
         // 1.Sort the string in descending order
         // 2.Find the first '1' from the right
         // 3.Swap it with the last element
@@ -12,5 +13,23 @@ public:
         swap(*it, s.back()); // swaping '1' to end of the string to make it odd binary
         
         return s;
+    }
+};
+
+//// This the approach 2 - :
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        //int size = s.length();
+        //1. count both number of the '1' and '0' 
+        //2. now we will be using the concatation adding the string of '1' with size count1 -1 and then string of '0' andding '1' at the end
+        int count1 =0  , count0 = 0;
+        for(int i = 0 ; i < s.length(); i++){
+            if(s[i] == '1') count1++;
+            if(s[i] == '0') count0++;
+        }
+        string ans;
+        ans = string(count1-1 , '1') + string(count0 , '0') + '1';
+        return ans;
     }
 };
